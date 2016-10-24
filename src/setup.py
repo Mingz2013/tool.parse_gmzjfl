8af5e485f0f100124ad6dfa7216d95403dda5718
mingz <305603665@qq.com>
2016-10-24 17:58:04 +0800

    init

---
+++ src/setup.py
@@ -0,0 +1,84 @@
+# -*- coding:utf-8 -*-
+__author__ = 'zhaojm'
+
+import os
+import codecs
+
+
+def do_line(line):
+    items = line.split()
+    # print items
+    if (items[0] >= 'A') and (items[0] <= 'T'):
+        # print len(items)
+        if len(items) == 3:
+            line = items[0] + "," + "," + "," + "," + items[1] + "," + items[2]
+            # print line
+        else:
+            print "error..........."
+    elif len(items[0]) == 2:
+        # print len(items)
+        if len(items) == 2:
+            line = "," + items[0] + "," + "," + "," + items[1] + ","
+            # print line
+        elif len(items) == 3:
+            line = "," + items[0] + "," + "," + "," + items[1] + "," + items[2]
+
+            # print line
+        else:
+            print "error..........."
+    elif len(items[0]) == 3:
+        # print len(items)
+        if len(items) == 2:
+            line = "," + "," + items[0] + "," + "," + items[1] + ","
+            # print line
+        elif len(items) == 3:
+            if len(items[1]) == 4 and ("0000" <= items[1] <= "9999"):
+                line = "," + "," + items[0] + "," + items[1] + "," + items[2] + ","
+            else:
+                line = "," + "," + items[0] + "," + "," + items[1] + "," + items[2]
+        elif len(items) == 4:
+            line = "," + "," + items[0] + "," + items[1] + "," + items[2] + "," + items[3]
+            # print line
+        else:
+            print "error..........."
+    elif len(items[0]) == 4:
+        # print len(items)
+        if len(items) == 2:
+            line = "," + "," + "," + items[0] + "," + items[1] + ","
+            # print line
+        elif len(items) == 3:
+            line = "," + "," + "," + items[0] + "," + items[1] + "," + items[2]
+            # print line
+        # elif len(items) == 4:
+        #     line = "," + "," + items[0] + "," + items[1] + "," + items[2] + "," + items[3]
+        #     # print line
+        else:
+            print "error...........%s" % len(items)
+    return line
+
+
+def do_dir(path):
+    out_str = ""
+
+    for file_name in os.listdir(path):
+        file_path = path + '/' + file_name
+        with codecs.open(file_path, encoding='utf-8') as f:
+            lines = f.readlines()
+            for line in lines:
+                l = do_line(line)
+                out_str += l + "\n"
+        pass
+    return out_str
+    pass
+
+
+def write_out(path, out_str):
+    with codecs.open(path, mode="w", encoding='utf-8') as f:
+        f.write(out_str)
+    pass
+
+
+if __name__ == "__main__":
+    out_str = do_dir('data')
+    write_out("out/out.csv", out_str)
+    pass
